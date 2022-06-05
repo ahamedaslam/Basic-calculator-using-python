32d239e8144751da5348bffe2e000b9bd34de67c
ahamedaslam <87639294+ahamedaslam@users.noreply.github.com>
2022-06-05 18:18:43 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter a number: '))
+operator = input('Enter a operator: ')
+
+if operator == '+':
+    print('The addition is',num1+num2)
+elif operator == '-':
+    print('The subtraction is',num1-num2)
+elif operator == '*':
+    print('The multiplication is',num1*num2)
+else:
+    if operator == '/':
+     print('The division is',num1/num2)
+
+
+
+
